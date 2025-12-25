e72f1d807250b7b32425133f5b97ea24002a0233
Ayush gupta <ayushgupta@researchedgeandpublication.com>
2025-12-25 10:44:16 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/dashboard/project-list.tsx
+++ src/components/dashboard/project-list.tsx
@@ -518,7 +518,7 @@ export function CreateProjectDialog({ userId }: { userId: string}) {
                                             <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                                         </Button>
                                     </FormControl>
-                                </Popover>
+                                </PopoverTrigger>
                                 <PopoverContent className="w-auto p-0" align="start">
                                     <Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus />
                                 </PopoverContent>
