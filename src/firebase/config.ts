47d018fae8013684cf8183ee9ab4c9dc5f462258
Ayush gupta <ayushgupta@researchedgeandpublication.com>
2025-12-25 11:17:05 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,12 @@
+
 // Your web app's Firebase configuration
 // IMPORTANT: Replace this with your actual Firebase config object
 export const firebaseConfig = {
+  projectId: "studio-811024150-b691e",
+  appId: "1:657512827836:web:b37c7cc881e15931f4b9b9",
   apiKey: "AIzaSyB3dqCoiJoZH8gOoAPNACS_eAIubLdBBd4",
   authDomain: "studio-811024150-b691e.firebaseapp.com",
-  projectId: "studio-811024150-b691e",
-  storageBucket: "studio-811024150-b691e.appspot.com",
+  measurementId: "",
   messagingSenderId: "657512827836",
-  appId: "1:657512827836:web:b37c7cc881e15931f4b9b9",
+  storageBucket: "studio-811024150-b691e.appspot.com",
 };
